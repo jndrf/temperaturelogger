d942a2da0fc031ba5528d6674cd1a73cbe5c5f18
Jonas Neundorf <jneun1@web.de>
2024-06-10 21:52:31 +0200

    read out 20 times and take average

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ ntc_pins = [32, 35]
 ntcs = [machine.ADC(pin, atten=3) for pin in ntc_pins]
 
 
+def adc_readout(adcs, n_reads=20):
+    '''average over several ntc readouts'''
+    if type(adcs) != list:
+        adcs = [adcs]
+
+    readouts = []
+    for adc in adcs:
+        readouts.append([adc.read() for i in range(n_reads)])
+
+    return [sum(e)/len(e) for e in readouts]
+
+
 while True:
     readings = [ntc.read() for ntc in ntcs]
     line = [time.time()]
